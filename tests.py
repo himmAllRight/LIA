5307f230de226da8cfcb7cba57d482621d7e67ce
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2016-12-06 07:03:52 -0500

    added test_parseLine()

--- tests.py
+++ tests.py
@@ -9,6 +9,14 @@ def test_parseHeader():
     expectedOutput = ['', '', '', 'date', '', 'amount', '', 'description', '']
     return(testOutput == expectedOutput)
     
+def test_parseLine():
+    header   = backend.parseHeader(",,date,amount,description,,")
+    lineData = backend.parseLine(",,09/06/1991,75.19,line statement,,", header)
+    expected =  {'description': 'line statement'
+                , 'date': '09/06/1991'
+                , 'amount': '75.19'}
+    return(lineData == expected)
+
 
 # Just until I write real ones
 def testTrue():
@@ -16,7 +24,8 @@ def testTrue():
 
 # Execute Tests
 testList =  [["True", testTrue()]
-            ,["parseHeader", test_parseHeader()]]
+             ,["parseHeader", test_parseHeader()]
+             ,["parseLine", test_parseLine()]]
 
 largestTestName =  functools.reduce(max, map(lambda x: len(x[0]), testList))
 
