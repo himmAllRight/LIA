4e1daffae93901f19724d1c1fef765a1df88f841
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2017-02-11 08:43:44 -0500

    Added recursive trim function for rule parsing

--- autoRules.py
+++ autoRules.py
@@ -1,11 +1,20 @@
 import liaBackend as backend
 
-
 ## Example rules line
-## description:Dunk -> description:DUNKINDONUTS|amount:15.00
+## description=Dunk -> Expenses:Food:Coffee
 
 def parseRuleLine(line, splitStr = "->", pairDelim= ":", delim= "|"):
     """Parses out data from line of rules file"""
     splitLine   = str.split(line, splitStr)
     matchPair   = str.split(splitLine[0], pairDelim)
     actionPairs = list(map(lambda x : str.split(x, ":"), str.split(splitLine[1], delim)))
+
+def recursiveTrim(nestedList):
+    """Recursively trims whitespace from strings in a nested list"""
+    returnList = []         ## I hate I have to do this with python
+    for curr in nestedList:
+        if(isinstance(curr, str)):
+            returnList.append(curr.strip())
+        elif(isinstance(curr, list)):
+            returnList.append(recursiveTrim(curr))
+    return(returnList)
