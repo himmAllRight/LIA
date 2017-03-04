00501026a742ba257bd47008734f1812d6fbb5d2
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2017-03-04 17:27:43 -0500

    Working on creating a setup.py. I followed the instructions but not sure if It installs correctly yet

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="LIA",
+        version="0.9",
+        packages=['lia'],
+        author="Ryan Himmelwright",
+        author_email="ryan.himmelwright@gmail.com",
+        url='ryan.himmelwright.net',
+        description="An assistant to help convert/import data to ledger-cli",
+        license="GPL-3.0",
+        keywords="ledger",        
+)
