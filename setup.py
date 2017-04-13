ad8571debae6b8c069ad53f7b24440d928cd1a16
Ryan <ryan@debian>
2017-04-12 21:28:49 -0400

    playing more with setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup, find_packages
 setup(
         name="LIA",
         version="0.9",
-        packages=['lia'],
         author="Ryan Himmelwright",
         author_email="ryan.himmelwright@gmail.com",
-        url='ryan.himmelwright.net',
         description="An assistant to help convert/import data to ledger-cli",
+        url='ryan.himmelwright.net',
         license="GPL-3.0",
+        
+        packages=['lia'],
+        scripts=['lia.py'],
         keywords="ledger",        
 )
